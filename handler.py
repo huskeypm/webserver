ed591c2b2275f1ebefe3a6c35cf52ff7b7efdcbb
Pete Kekenes-Huskey <Pete Kekenes-Huskey@localhost>
2017-07-19 12:38:33 -0400

    added

---
+++ handler.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import sys
+##################################
+#
+# Revisions
+#       10.08.10 inception
+#
+##################################
+
+#
+# ROUTINE  
+#
+def doit(allargs): 
+  pdb = allargs[1]
+  param= allargs[2]
+  out = allargs[3]
+  
+  print pdb, param, out 
+
+
+#
+# Message printed when program run without arguments 
+#
+def helpmsg():
+  scriptName= sys.argv[0]
+  msg="""
+Purpose: 
+ 
+Usage:
+"""
+  msg+="  %s -validation" % (scriptName)
+  msg+="""
+  
+ 
+Notes:
+
+"""
+  return msg
+
+#
+# MAIN routine executed when launching this script from command line 
+#
+if __name__ == "__main__":
+  import sys
+  msg = helpmsg()
+  remap = "none"
+
+  if len(sys.argv) < 2:
+      raise RuntimeError(msg)
+
+  #fileIn= sys.argv[1]
+  #if(len(sys.argv)==3):
+  #  1
+  #  #print "arg"
+
+  # Loops over each argument in the command line 
+  for i,arg in enumerate(sys.argv):
+    # calls 'doit' with the next argument following the argument '-validation'
+    if(arg=="-validation"):
+      arg1=sys.argv[i+1] 
+      doit(arg1)
+  
+
+
+
+
+  doit(sys.argv)
+  #raise RuntimeError("Arguments not understood")
+
+
+
+
